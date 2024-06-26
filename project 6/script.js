4dd3e9a82e1f5fc1c14bbcd259b0df3f141e2aba
ritesh mishra <riteshmishra9565@gmail.com>
2024-06-26 08:32:34 +0530

    done

--- project 6/script.js
+++ project 6/script.js
@@ -1,5 +1,23 @@
-btn = document.querySelector('[btn]')
-updateText = document.querySelector('[update-text]')
+btn = document.querySelector("button");
+updateText = document.querySelector("[update-text]");
 
+// // btn.addEventListener('click', () => {
+// //     updateText.innerText = 'jai ho';
+// //   });
 
-btn.add
+// btn.addEventListener("click", () => {
+//   if (updateText === "stranger") {
+//     updateText.textContent = `you'r friend now`;
+//     btn.textContent = "remove";
+//   }
+// });
+
+btn.addEventListener("click", () => {
+  if (btn.textContent === "add friend") {
+    btn.textContent = "remove friend";
+    updateText.textContent = "friend added";
+  } else {
+    btn.textContent = "add friend";
+    updateText.textContent = "stranger";
+  }
+});
