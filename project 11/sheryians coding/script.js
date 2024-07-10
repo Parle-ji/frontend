bf29b312b0bf480bf159d30ca8e467cbff966b59
ritesh mishra <riteshmishra9565@gmail.com>
2024-07-10 10:14:47 +0530

    project 11 pe mujhe cdn ke taraf se error aa rha haii...

---
+++ project 11/sheryians coding/script.js
@@ -0,0 +1,12 @@
+alert()
+// Shery.imageEffect(".back", {style:5, debug:true});
+// try{
+//     Shery.imageEffect(".back", {style:5, debug:true})
+// }catch(e){
+//     alert(e)
+// }
+try{
+    Shery.imageEffect('.back', { style:5, debuge:true})
+}catch(e){
+    alert(e)
+}
